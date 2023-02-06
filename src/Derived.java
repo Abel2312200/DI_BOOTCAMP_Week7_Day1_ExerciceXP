c460857306a054fff4dc58a2ceaf32a4cd87457f
Abel Ngbandaman <ngbandaman2022@gmail.com>
2023-02-06 16:50:16 +0000

    Exercise 3 : done

--- src/Derived.java
+++ src/Derived.java
@@ -1,2 +1,71 @@
-public class Derived {
+//Base Vehicle class
+class Vehicle {
+    // class attributes
+    protected String model;
+    protected double speed;
+
+    // constructor without parameters
+    public Vehicle() {
+        super();
+    }
+    // constructor(s) using parameter(s)
+    public Vehicle(String model, double speed) {
+        this.model = model;
+        this.speed = speed;
+    }
+
+    // getters and setters
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "model='" + model + '\'' +
+                ", speed=" + speed +
+                '}';
+    }
+}
+
+// Derived Class
+class Car extends Vehicle {
+
+    private String name;
+
+    // Default Constructor
+    public Car() {
+        super();
+    }
+
+    // constructor(s) using parameter(s)
+    public Car(String model, double speed, String name){
+        super(model,speed);
+        this.name = name;
+    }
+
+    // This function sets the name of the car
+    public void setDetails(String name) { // Setter Function
+        this.name = name;
+    }
+
+    // This function calls the Base class functions and appends the result with the input
+    public String getDetails(String carName) {
+        // calling Base Class Function
+        String message = "Name "+carName+super.toString();
+        return (message);
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,15 @@ public class Main {
         // Exercise 1
         MyPoint p1 = new MyPoint(3, 4);
         System.out.println(p1.distance(5, 6));
+
+        // Exercice 2
+        Person person = new Person("John", "Wick", "john@wick.com", 55);
+        String infoPerson = person.toString();
+        System.out.println(infoPerson);
+
+        // Exercise 3
+        Car car = new Car("Tesla",100,"Rooster");
+        String detailsCar = car.getDetails("Rooster");
+        System.out.println(detailsCar);
     }
 }
